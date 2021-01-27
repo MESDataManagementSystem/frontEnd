b01a9616a0e5fc0b2c84c0afbbe90e7a0e26ded4
aparece1241 <apareceh7@gmail.com>
2021-01-27 21:30:52 +0800

    Initial input every page

--- MESDM-Interface/src/app/app-routing.module.ts
+++ MESDM-Interface/src/app/app-routing.module.ts
@@ -3,12 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent} from './dashboard/dashboard.component';
+import { AllStudentsComponent } from './all-students/all-students.component';
+import { AllTeachersComponent } from './all-teachers/all-teachers.component';
+import { ClassesComponent } from './classes/classes.component';
+
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full'},
   { path: 'login', component: LoginComponent},
   { path: 'dashboard', component: DashboardComponent},
+  { path: 'allStudents', component: AllStudentsComponent},
+  { path: 'classes', component: ClassesComponent},
+  { path: 'allTeachers', component: AllStudentsComponent}
 ];
 
 @NgModule({
--- MESDM-Interface/src/app/app.module.ts
+++ MESDM-Interface/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+
 // import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 
@@ -8,11 +9,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AllStudentsComponent } from './all-students/all-students.component';
+import { AllTeachersComponent } from './all-teachers/all-teachers.component';
+import { ClassesComponent } from './classes/classes.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    DashboardComponent
+    DashboardComponent,
+    NavBarComponent,
+    AllStudentsComponent,
+    AllTeachersComponent,
+    ClassesComponent
   ],
   imports: [
     BrowserModule,
