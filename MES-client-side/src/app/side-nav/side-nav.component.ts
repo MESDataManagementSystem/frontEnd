51c08a3139b4f364bfb1ee66da16fb52476df531
aparece1241 <apareceh7@gmail.com>
2021-02-03 18:00:36 +0800

    using material design nav-bar initial

---
+++ MES-client-side/src/app/side-nav/side-nav.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+// import { Content } from '@angular/compiler/src/render3/r3_ast';
+
+@Component({
+  selector: 'app-side-nav',
+  templateUrl: './side-nav.component.html',
+  styleUrls: ['./side-nav.component.css']
+})
+export class SideNavComponent implements OnInit {
+  navContent: Array <string> = ['Dashboard', 'Students', 'Teachers', 'Classes', 'Setting'];
+  showSideNav = true;
+  lateActive = 'Dashboard';
+  oldBgColor = '#e3e6e8';
+
+  constructor( private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+  }
+
+  childRoutes(content): void{
+    const routes = content.toLowerCase();
+    this.router.navigate([routes], {relativeTo: this.route});
+
+    // }
+    this.lateActive = content;
+  }
+
+}
