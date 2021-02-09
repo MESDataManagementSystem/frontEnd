b3dacba99f3ea55c7c90b1cad59cd0875940e7e7
Luzmibel <luzmibel.paculanag@student.passerellesnumeriques.org>
2021-02-09 15:14:44 +0800

    Initial Commit

--- MES-client-side/src/app/app.module.ts
+++ MES-client-side/src/app/app.module.ts
@@ -5,15 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatIconModule} from '@angular/material/icon';
-import {MatListModule} from '@angular/material/list';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatTableModule} from '@angular/material/table';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatCardModule} from '@angular/material/card';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatCardModule } from '@angular/material/card';
 
 import { AllStudentsComponent } from './all-students/all-students.component';
 import { AllTeachersComponent } from './all-teachers/all-teachers.component';
@@ -21,7 +20,6 @@ import { ClassesComponent } from './classes/classes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
-
 
 @NgModule({
   declarations: [
@@ -44,8 +42,7 @@ import { SideNavComponent } from './side-nav/side-nav.component';
     MatInputModule,
     MatFormFieldModule,
     MatTooltipModule,
-    MatCardModule
-
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
