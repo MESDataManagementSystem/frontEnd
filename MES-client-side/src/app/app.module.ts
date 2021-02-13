beaa2d2686077c41330e4f9587d6d448e362a642
Luzmibel <luzmibel.paculanag@student.passerellesnumeriques.org>
2021-02-13 14:11:41 +0800

    Pull from origin Rufo

--- MES-client-side/src/app/app.module.ts
+++ MES-client-side/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -48,7 +47,8 @@ import { SideNavComponent } from './side-nav/side-nav.component';
     MatFormFieldModule,
     MatTooltipModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
